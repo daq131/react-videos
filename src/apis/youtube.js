a5987776d040b4d14f8f2b3c5024cbf9d3c12702
Dawid Szarwark <daq131@gmail.com>
2022-07-18 20:59:49 +0200

    add youtube api & yt component

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY =  'AIzaSyD18TbzbA3Nkt_5lcB4HaIPL8DchO7wXzY';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
