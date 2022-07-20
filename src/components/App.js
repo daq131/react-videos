7ecc1edd170ebcacf029b0d41a0cb4baeaec19c8
Dawid Szarwark <daq131@gmail.com>
2022-07-20 13:15:54 +0200

    refactor code add hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
 
-  componentDidMount() {
-    this.onTermSubmit('cars');
-  }
+const App = () => {
+  const[videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  onTermSubmit = async (term) => {
+  useEffect( () => {
+    onTermSubmit('cars');
+
+  }, []);
+
+  const onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({ 
-        videos: response.data.items,
-        selectedVideo: response.data.items[0]
-    });
-  };
 
-  onVideoSelect = (video) => {
-    this.setState({selectedVideo: video});
-}
+    setVideos(response.data.items);
+    selectedVideo(response.data.items[0])
+  }
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
+  }
 
-  render() {
+  
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={onTermSubmit} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
-                        <VideoDetail video={this.state.selectedVideo} />
+                        <VideoDetail video={selectedVideo} />
                     </div>
                     <div className="fice wild column">
-                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />     
+                        <VideoList onVideoSelect={onVideoSelect} videos={videos} />     
                     </div>
                 </div>
         </div>
       </div>
     );
-  }
 }
+
 
 export default App;
